d76704af29ea55cb9a0d270cbb5661c1153eb6f8
Julian Paris <Julian.Pariss@hotmail.com>
2024-10-22 15:03:41 -0300

    logo animation

--- src/components/organisms/footer/index.jsx
+++ src/components/organisms/footer/index.jsx
@@ -12,6 +12,15 @@ const Footer = () => {
         <div className='ft-conatiner'>
             <div className='ft-topDivider'/>
             <div className='ft-content'>
+                <div className='ft-logo-container'>
+                    <div className='ft-logo-light'/>
+                    <div className='ft-logo-gif'/>
+                    <div className='ft-logo-border'>                        
+                        <img src='Logo.png' className='ft-logo-icon' alt='zero icon'/>
+                        <h1 className='ft-logo-title baseColor'>ZERO</h1>
+                        <h1 className="ft-logo-title backgroundBlend">ZERO</h1> 
+                    </div>
+                </div>
                 <div className='ft-navBar'>
                     <div className='ft-bar'>
                         <div className='ft-bar-icon-container' onClick={() => {window.scrollTo({top: 0, behavior: 'smooth'})}}>
